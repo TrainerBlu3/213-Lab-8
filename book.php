c69fa0360a032385bae1e3bed4aaa347dabb7016
Matthew Tindogan <77546248+TrainerBlu3@users.noreply.github.com>
2025-11-05 15:57:13 -0800

    Enhance Book class with price management
    Added price property and methods to manage book pricing.

--- book.php
+++ book.php
@@ -1,21 +1,33 @@
-<? php
+<?php
 
 class Book{
 
   public $title;
-  public $author
+  public $author;
+  private $price;
 
-  public function __construct($title, $author){
+  public function __construct($title, $author, $price = 0){
     $this -> title = $title;
     $this -> author = $author;
+    $this -> price = $price;
   }
 
   public function getDetails(){
-    echo "{$this->title} by {$this->author}";
+    return "{$this->title} by {$this->author}";
   }
 
   public function __destruct(){
     echo "Book object destroyed<br>";
+  }
+
+  public function setPrice($p){
+    $this -> price = $p;
+  }
+
+  public function getPrice(){
+    return "{$this->title} costs \${$this->price}<br>";
+  }
+  
 }
 
 $book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
@@ -27,5 +39,13 @@ echo $book2->getDetails();
 $book3 = new Book("Tarzan", "Edgar Rice Burroughs");
 echo $book3->getDetails() . "<br>";
 unset($book3);
+
+$book4 = new Book("Dune", "Frank Herbert", 19.99);
+
+echo $book4->getDetails() . "<br>";
+echo $book4->getPrice() . "<br>";
+
+$book4->setPrice(24.99);
+echo $book4->getPrice() . "<br>";
 
 ?>
