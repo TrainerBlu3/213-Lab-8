9df34b02511f86e5f3a5a886afb57fad292efba5
Matthew Tindogan <77546248+TrainerBlu3@users.noreply.github.com>
2025-11-05 15:50:23 -0800

    Implement destructor in Book class
    Added a destructor to the Book class and adjusted formatting.

--- book.php
+++ book.php
@@ -10,9 +10,12 @@ class Book{
     $this -> author = $author;
   }
 
-public function getDetails(){
-  echo "{$this->title} by {$this->author}";
-}
+  public function getDetails(){
+    echo "{$this->title} by {$this->author}";
+  }
+
+  public function __destruct(){
+    echo "Book object destroyed<br>";
 }
 
 $book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
@@ -20,5 +23,9 @@ $book2 = new Book("The Way of Kings", "Brandon Sanderson");
 
 echo $book1->getDetails() . "<br>";
 echo $book2->getDetails();
+
+$book3 = new Book("Tarzan", "Edgar Rice Burroughs");
+echo $book3->getDetails() . "<br>";
+unset($book3);
 
 ?>
